0eba7280eaab1cf5ad7c652c631b297e33120f54
herambchaudhari4121 <heramb.chaudhri@gmail.com>
2025-10-04 16:52:52 +0530

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,247 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Redux",
+    image: "redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Framer Motion",
+    image: "framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Stripe",
+    image: "stripe.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/",
+  },
+  {
+    name: "Facebook",
+    icon: FaFacebook,
+    link: "https://www.holopin.io/@heramb#badges",
+  },
+  {
+    name: "Twitter",
+    icon: RxTwitterLogo,
+    link: "https://x.com/Heramb24",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+
+
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+
+
+] as const;
+
+export const FULLSTACK_SKILL = [
+] as const;
+
+export const OTHER_SKILL = [
+
+] as const;
+
+export const PROJECTS = [
+  {
+    title: "ClinicVoice – AI-Powered Healthcare Assistant",
+    image: "/projects/skills/clinice.png",
+    link: "https://clinic-voice-agent.vercel.app/#/dashboard",
+  },
+  {
+    title: "Dribbble-inspired project - Design. Share. Inspire.",
+    image: "/projects/skills/dribbleclone.png",
+    link: "https://driblle-clone.netlify.app/",
+  },
+  {
+    title: "SecureURL – Real-Time Protection from Phishing",
+    image: "/projects/skills/secureurl.png",
+    link: "https://cozy-choux-fa9c1d.netlify.app/",
+  },
+] as const;
+
+
+export const FOOTER_DATA = [
+  {
+    title: "Community",
+    data: [
+
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/herambchaudhari4121",
+      },
+
+    ],
+  },
+  {
+    title: "Social Media",
+    data: [
+
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "https://www.linkedin.com/in/heramb-chaudhari-83a01821a/",
+      },
+    ],
+  },
+  {
+    title: "About",
+    data: [
+   
+      {
+        name: "Contact Me",
+        icon: null,
+        link: "Heramb.Chaudhri@gmail.com",
+      },
+    ],
+  },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+] as const;
+
+
